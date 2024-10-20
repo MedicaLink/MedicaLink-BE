316fdefa7ccbb02a0bc2c411b70330498b2198fa
Nisala Dulanaka <dulanakanisala654@gmail.com>
2024-10-20 19:40:59 +0530

    env configuration code improvement

--- src/main/java/com/medicalink/MedicaLink_backend/MedicaLinkBackendApplication.java
+++ src/main/java/com/medicalink/MedicaLink_backend/MedicaLinkBackendApplication.java
@@ -10,13 +10,9 @@ public class MedicaLinkBackendApplication {
 	public static void main(String[] args) {
 
 		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USER", dotenv.get("DB_USER"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-		System.setProperty("AUTH_KEY", dotenv.get("AUTH_KEY"));
-		System.setProperty("FHIR_URL", dotenv.get("FHIR_URL"));
+		dotenv.entries().forEach((entry) -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
 
 		SpringApplication.run(MedicaLinkBackendApplication.class, args);
 	}
